e3bf4d8c893ade961198da3356bd814a7e7989ad
Nan Jiang <johnny.nan.jiang@gmail.com>
2018-12-16 10:06:05 +1100

    - refactor Sector class and its tests

--- src/test/java/au/com/jnj/asx/sector/SectorTest.kt
+++ src/test/java/au/com/jnj/asx/sector/SectorTest.kt
@@ -1,12 +1,9 @@
 package au.com.jnj.asx.sector
 
-import jdk.nashorn.internal.parser.JSONParser
-import org.json.JSONObject
-import org.jsoup.Jsoup
 import org.junit.Test
-import org.jsoup.nodes.Document
-import org.jsoup.select.Elements
 import org.junit.Assert.*
+import java.io.*
+import java.nio.charset.Charset
 
 /**
  * Created by nanjiang on 15/12/18.
@@ -14,31 +11,29 @@ import org.junit.Assert.*
 
 class SectorTest {
     @Test
-    fun testGettingSectorPerformance() {
-        val doc: Document = Jsoup.connect("https://au.spindices.com/indices/equity/sp-asx-200-utilities-sector").timeout(10 * 1000).get()
-        val bodyTag = doc.getElementsByTag("body")[0] // TODO: get index data from body text directly for fault tolerance
-        val scriptTags = doc.getElementsByTag("script")
-        val scriptTagWithData = scriptTags[11]
-        val scriptCodeBlock = scriptTagWithData.childNodes()[0].attributes().get("data")
-        val indexDataString = scriptCodeBlock.substringAfter("var indexData = ").substringBefore(";")
-        val jsonObject = JSONObject(indexDataString)
-        val indexPerformanceHolder = jsonObject.get("indexPerformanceHolder") as JSONObject
-        val indexPerformance = indexPerformanceHolder.get("indexPerformance") as JSONObject
-        val priceReturn = indexPerformance.get("priceReturn") as JSONObject
-        val dailyReturn = priceReturn.get("dailyReturn") as Double
-        val monthToDateReturn = priceReturn.get("monthToDateReturn") as Double
-        val quarterToDateReturn = priceReturn.get("quarterToDateReturn") as Double
-        val yearToDateReturn = priceReturn.get("yearToDateReturn") as Double
-        val oneYearReturn = priceReturn.get("oneYearReturn") as Double
-        val threeYearReturn = priceReturn.get("threeYearReturn") as Double
-        val fiveYearReturn = priceReturn.get("fiveYearReturn") as Double
-        val tenYearReturn = priceReturn.get("tenYearReturn") as Double
+    fun testGettingJsonDataFromUrl() {
+        val indexJsonDataString = Sector.getJsonStringFromUrl("https://au.spindices.com/indices/equity/sp-asx-200-utilities-sector")
 
-        assertEquals(23, scriptTags.size)
-        assertTrue(indexDataString.startsWith("{\"status\":true,"))
-        assertTrue(oneYearReturn.toString().startsWith("-13.56"))
-        assertTrue(threeYearReturn.toString().startsWith("3.55"))
-        assertTrue(fiveYearReturn.toString().startsWith("6.67"))
-        assertTrue(tenYearReturn.toString().startsWith("6.116"))
+        assertTrue(indexJsonDataString.startsWith("{\"status\":true,"))
+    }
+
+    @Test
+    fun testGettingSectorPerformance() {
+        val indexJsonDataString = TestUtils.loadFileToString("index_data_for_industry_sector.json")
+        val sectorPerformance = Sector.getSectorPerformanceDetailsFromJsonString(indexJsonDataString)
+
+        assertTrue(sectorPerformance.returns.oneYearReturn.toString().startsWith("-13.56"))
+        assertTrue(sectorPerformance.returns.threeYearReturn.toString().startsWith("3.55"))
+        assertTrue(sectorPerformance.returns.fiveYearReturn.toString().startsWith("6.67"))
+        assertTrue(sectorPerformance.returns.tenYearReturn.toString().startsWith("6.116"))
+    }
+}
+
+class TestUtils {
+    companion object {
+        fun loadFileToString(filepath: String, charset: Charset = Charsets.ISO_8859_1) : String {
+            return File(this.javaClass.classLoader.getResource(filepath).file)
+                    .readText(charset)
+        }
     }
 }
