7f04af535d8719c94cac372e6f74c5448936ae5b
Nan Jiang <johnny.nan.jiang@gmail.com>
2018-12-16 01:55:15 +1100

    - implement get ? period returns

--- src/test/java/au/com/jnj/asx/sector/SectorTest.kt
+++ src/test/java/au/com/jnj/asx/sector/SectorTest.kt
@@ -1,5 +1,7 @@
 package au.com.jnj.asx.sector
 
+import jdk.nashorn.internal.parser.JSONParser
+import org.json.JSONObject
 import org.jsoup.Jsoup
 import org.junit.Test
 import org.jsoup.nodes.Document
@@ -18,9 +20,25 @@ class SectorTest {
         val scriptTags = doc.getElementsByTag("script")
         val scriptTagWithData = scriptTags[11]
         val scriptCodeBlock = scriptTagWithData.childNodes()[0].attributes().get("data")
-        val indexDataText = scriptCodeBlock.substringAfter("var indexData = ").substringBefore(";")
+        val indexDataString = scriptCodeBlock.substringAfter("var indexData = ").substringBefore(";")
+        val jsonObject = JSONObject(indexDataString)
+        val indexPerformanceHolder = jsonObject.get("indexPerformanceHolder") as JSONObject
+        val indexPerformance = indexPerformanceHolder.get("indexPerformance") as JSONObject
+        val priceReturn = indexPerformance.get("priceReturn") as JSONObject
+        val dailyReturn = priceReturn.get("dailyReturn") as Double
+        val monthToDateReturn = priceReturn.get("monthToDateReturn") as Double
+        val quarterToDateReturn = priceReturn.get("quarterToDateReturn") as Double
+        val yearToDateReturn = priceReturn.get("yearToDateReturn") as Double
+        val oneYearReturn = priceReturn.get("oneYearReturn") as Double
+        val threeYearReturn = priceReturn.get("threeYearReturn") as Double
+        val fiveYearReturn = priceReturn.get("fiveYearReturn") as Double
+        val tenYearReturn = priceReturn.get("tenYearReturn") as Double
 
         assertEquals(23, scriptTags.size)
-        assertTrue(indexDataText.startsWith("{\"status\":true,"))
+        assertTrue(indexDataString.startsWith("{\"status\":true,"))
+        assertTrue(oneYearReturn.toString().startsWith("-13.56"))
+        assertTrue(threeYearReturn.toString().startsWith("3.55"))
+        assertTrue(fiveYearReturn.toString().startsWith("6.67"))
+        assertTrue(tenYearReturn.toString().startsWith("6.116"))
     }
 }
