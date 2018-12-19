7fbe8940b0afe1895a9e9c928fce235ef87ec94d
Nan Jiang <johnny.nan.jiang@gmail.com>
2018-12-19 18:56:25 +1100

    - implement formatting double

--- src/main/java/au/com/jnj/asx/sector/SectorPerformance.kt
+++ src/main/java/au/com/jnj/asx/sector/SectorPerformance.kt
@@ -1,47 +1,53 @@
 package au.com.jnj.asx.sector
 
+import java.text.DecimalFormat
+import java.text.NumberFormat
+
 /**
  * Created by nanjiang on 16/12/18.
  */
 
 data class SectorPerformance(val sectorDefinition: SectorDefinitions,
-                             val datetime: Double = 0.0,
+                             val datetime: Double = 0.00,
                              val returns: Returns) {
 
-    data class Returns(val dailyReturn: Double = 0.0,
-                       val monthToDateReturn: Double = 0.0,
-                       val quarterToDateReturn: Double = 0.0,
-                       val yearToDateReturn: Double = 0.0,
-                       val oneYearReturn: Double = 0.0,
-                       val threeYearReturn: Double = 0.0,
-                       val fiveYearReturn: Double = 0.0,
-                       val tenYearReturn: Double = 0.0)
+    data class Returns(val dailyReturn: Double = 0.00,
+                       val monthToDateReturn: Double = 0.00,
+                       val quarterToDateReturn: Double = 0.00,
+                       val yearToDateReturn: Double = 0.00,
+                       val oneYearReturn: Double = 0.00,
+                       val threeYearReturn: Double = 0.00,
+                       val fiveYearReturn: Double = 0.00,
+                       val tenYearReturn: Double = 0.00)
 
-    // the following getters are workaround to get CSV BeanWriter to read fields
+    private fun formatDouble(double: Double): Double =
+            DecimalFormat("#.##").format(double).toDouble()
+
+    // the following getters are a workaround to get CSV BeanWriter to read fields
     val name: String
         get() = this.sectorDefinition.description
 
     val dailyReturn: Double
-        get() = this.returns.dailyReturn
+        get() = formatDouble(this.returns.dailyReturn)
 
     val monthToDateReturn: Double
-        get() = this.returns.monthToDateReturn
+        get() = formatDouble(this.returns.monthToDateReturn)
 
     val quarterToDateReturn: Double
-        get() = this.returns.quarterToDateReturn
+        get() = formatDouble(this.returns.quarterToDateReturn)
 
     val yearToDateReturn: Double
-        get() = this.returns.yearToDateReturn
+        get() = formatDouble(this.returns.yearToDateReturn)
 
     val oneYearReturn: Double
-        get() = this.returns.oneYearReturn
+        get() = formatDouble(this.returns.oneYearReturn)
 
     val threeYearReturn: Double
-        get() = this.returns.threeYearReturn
+        get() = formatDouble(this.returns.threeYearReturn)
 
     val fiveYearReturn: Double
-        get() = this.returns.fiveYearReturn
+        get() = formatDouble(this.returns.fiveYearReturn)
 
     val tenYearReturn: Double
-        get() = this.returns.tenYearReturn
+        get() = formatDouble(this.returns.tenYearReturn)
 }
--- src/test/java/au/com/jnj/asx/sector/SectorsTest.kt
+++ src/test/java/au/com/jnj/asx/sector/SectorsTest.kt
@@ -10,7 +10,7 @@ import org.junit.Test
 
 class SectorsTest {
     @Test
-    //@Ignore
+    @Ignore
     fun testGettingPerformanceList() {
         val sectors = Sectors()
 
@@ -25,29 +25,31 @@ class SectorsTest {
         val performanceList = listOf(
                 SectorPerformance(
                         SectorDefinitions.A_REIT,
-                        returns = SectorPerformance.Returns(1.1, 1.2, 1.3, 1.4, 1.5, 1.6, 1.7, 1.8)),
+                        returns = SectorPerformance.Returns(1.1, 1.2, 1.3, 1.4, 1.5, 1.6, 1.7, 1.8153)),
                 SectorPerformance(
                         SectorDefinitions.CONSUMER_STAPLES,
-                        returns = SectorPerformance.Returns(2.1, 2.2, 2.3, 2.4, 2.5, 2.6, 2.7, -2.8)),
+                        returns = SectorPerformance.Returns(2.1, 2.2, 2.3, 2.4, 2.5, 2.6, 2.7, -2.8123)),
                 SectorPerformance(
                         SectorDefinitions.COMMUNICATION_SERVICES,
-                        returns = SectorPerformance.Returns(2.1, 2.2, 2.3, 2.4, 2.5, 2.66, 2.7, -2.8)),
+                        returns = SectorPerformance.Returns(2.1, 2.2, 2.3, 2.4, 2.5, 2.66, 2.7, -2.8123)),
                 SectorPerformance(
                         SectorDefinitions.ENERGY,
-                        returns = SectorPerformance.Returns(2.11, 2.2, 2.3, 2.4, 2.5, 2.66, 2.7, -2.8)),
+                        returns = SectorPerformance.Returns(2.11, 2.2, 2.3, 2.4, 2.5, 2.66, 2.7, -2.8123)),
                 SectorPerformance(
                         SectorDefinitions.FINANCIAL,
-                        returns = SectorPerformance.Returns(3.1, 3.2, 3.3, 3.4, 3.5, 3.6, 3.7, 3.8)))
+                        returns = SectorPerformance.Returns(3.1, 3.2, 3.3, 3.4, 3.5, 3.6, 3.7, 3.8123)))
 
         val sectors = Sectors()
         sectors.getPerformanceListInCSV(
                 sectors.sortPerformanceList(performanceList)
         )
 
-        assertTrue(performanceList[0].sectorDefinition.equals(SectorDefinitions.FINANCIAL))
-        assertTrue(performanceList[1].sectorDefinition.equals(SectorDefinitions.A_REIT))
-        assertTrue(performanceList[2].sectorDefinition.equals(SectorDefinitions.ENERGY))
-        assertTrue(performanceList[3].sectorDefinition.equals(SectorDefinitions.COMMUNICATION_SERVICES))
-        assertTrue(performanceList[4].sectorDefinition.equals(SectorDefinitions.CONSUMER_STAPLES))
+        assertTrue(performanceList[0].name.equals(SectorDefinitions.FINANCIAL))
+        assertTrue(performanceList[1].name.equals(SectorDefinitions.A_REIT))
+        assertTrue(performanceList[2].name.equals(SectorDefinitions.ENERGY))
+        assertTrue(performanceList[3].name.equals(SectorDefinitions.COMMUNICATION_SERVICES))
+        assertTrue(performanceList[4].name.equals(SectorDefinitions.CONSUMER_STAPLES))
+
+        assertEquals(1.82, performanceList[1].tenYearReturn, 0.0)
     }
 }
